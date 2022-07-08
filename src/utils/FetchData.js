a969d2587dc352ac76a414e3278aa18684ba7a85
amindraa05 <60906676+amindraa05@users.noreply.github.com>
2022-07-08 10:44:40 +0700

    Fixed All

---
+++ src/utils/FetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions = {
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  headers: {
+    "X-RapidAPI-Key": "1060a4bc98mshabfda103e8cbddbp18de8ejsn0c5f73310db3",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
